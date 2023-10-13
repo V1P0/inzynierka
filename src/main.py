96fa87444b6af9cf0a3371e900c3f0b799639b0b
261691 <261691@student.pwr.edu.pl>
2023-10-13 08:53:14 +0200

    update

--- src/main.py
+++ src/main.py
@@ -1,7 +1,20 @@
-
+def minPair(a, b):
+    if(a[0] > b[0]):
+        return (a, b)
+    elif(a[0] < b[0]):
+        return (b, a)
+    elif(a[1] > b[1]):
+        return (a, b)
+    else:
+        return (b, a)
 
 def main():
-    print("Hello World!")
+    width = 9
+    height = 13
+    for j in range(1, height-1):
+        print(minPair((0, j), (width-1, j)), end=', ')
+    for i in range(1, 3):
+        print(minPair((i, 1), (i, height-2)), end=', ')
 
 
 if __name__ == '__main__':
